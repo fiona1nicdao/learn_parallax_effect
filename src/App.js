b4aa55d67e6246f16cd05727059517d37ffb3c83
FionaN <fiona1941nicdao@gmail.com>
2023-02-16 22:01:39 -0600

    add parallax code to test scrolling up and down

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0} speed={1} factor={1.5} className="toplayer">
+          <h1>Welcome to my website</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0} 
+        // onClick={()=>ref.current.scrollTo(3)}
         >
-          Learn React
-        </a>
-      </header>
+          <h2>Web developement is fun! </h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
